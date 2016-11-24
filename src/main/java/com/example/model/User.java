f4cdf3b5dd36da09b896ec8204419b64d5bd9a1e
LeeChangYong <lcy9002@naver.com>
2016-11-25 00:00:26 +0900

    modify

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -4,11 +4,15 @@ package com.example.model;
  * Created by LichKing on 2016. 11. 24..
  */
 public class User {
+    private Integer userNo;
     private String userId;
     private String password;
     private String name;
     private String email;
 
+    public void setUserNo(Integer userNo) {
+        this.userNo = userNo;
+    }
     public void setUserId(String userId) {
         this.userId = userId;
     }
@@ -23,6 +27,26 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Integer getUserNo() {
+        return userNo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     @Override
--- src/main/java/com/example/web/UserController.java
+++ src/main/java/com/example/web/UserController.java
@@ -3,9 +3,7 @@ package com.example.web;
 import com.example.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +18,31 @@ public class UserController {
     @PostMapping("/user/create")
     public String create(User user){
         System.out.println("userId :: " + user);
+        user.setUserNo(userList.size());
         userList.add(user);
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String list(Model model){
+        model.addAttribute("users", userList);
+        return "/user/list";
+    }
+
+    @GetMapping("/users/{id}/form")
+    public String modify(@PathVariable Integer id, Model model){
+        model.addAttribute("user", userList.get(id));
+        System.out.println(userList.get(id));
+        return "/user/updateForm";
+    }
+
+    @PostMapping("/users/{id}/update")
+    public String update(@PathVariable Integer id, User user, Model model){
+        if(userList.get(id).getPassword().equals(user.getPassword())){
+            user.setUserNo(id);
+            userList.set(id, user);
+        }
+
         model.addAttribute("users", userList);
         return "/user/list";
     }
